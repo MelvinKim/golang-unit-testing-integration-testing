0ff1b5c1b24946f76967cac953f4f2d553e4b2c2
MelvinKim <melobruk1@gmail.com>
2023-01-12 08:47:27 +0300

    feat: create a middleware to get the IP of the client request

---
+++ webapp/cmd/web/middleware.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type contextKey string
+
+const contextUserKey contextKey = "user_ip"
+
+func (app *application) ipFromContext(ctx context.Context) string {
+	return ctx.Value(contextUserKey).(string)
+}
+
+func (app *application) addIpToContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// we need to have a context to work with
+		var ctx = context.Background()
+
+		// get the ip (as accurately as possible)
+		// ip := r.RemoteAddr, returns the IP address of the request , not always accurate especially when the client is behind a proxy
+	})
+}
+
+func getIP(r *http.Request) (string, error) {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "unknown", err
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return "", fmt.Errorf("userip: %q is not IP:port", r.RemoteAddr)
+	}
+
+	// check if the IP  is coming from a proxy
+	forward := r.Header.Get("X-Forwarded-For")
+	if len(forward) > 0 {
+		ip = forward
+	}
+
+	if len(ip) == 0 {
+		ip = "forward"
+	}
+
+	return ip, nil
+}
