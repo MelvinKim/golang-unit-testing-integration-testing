6fa7a84b9d178e07598797a39fba78f64de7efaf
MelvinKim <melobruk1@gmail.com>
2023-01-12 08:02:32 +0300

    Testing application handlers

---
+++ webapp/cmd/web/handlers_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_handlers(t *testing.T) {
+	var theTests = []struct {
+		name               string
+		url                string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"404", "/fish", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	// creates a test server
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	// range through the tests
+	for _, e := range theTests {
+		resp, err := ts.Client().Get(ts.URL + e.url)
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("for %s: expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+		}
+	}
+}
